f52249f1a21e4bba609af5e970ac4cd61772e21b
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-03-31 13:55:05 +0530

    Initialize a private field of Connection type with null value.

--- src/com/wipro/candidate/util/DBUtil.java
+++ src/com/wipro/candidate/util/DBUtil.java
@@ -1,9 +1,13 @@
 package com.wipro.candidate.util;
 
+import java.sql.Connection;
+
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/Candidate Management System";
 
     private static final String USERNAME = "root";
 
     private static final String PASSWORD = "";
+
+    private static Connection connection = null;
 }
