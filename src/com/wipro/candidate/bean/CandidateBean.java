e0defa538ca808e8c9a0c1288d2ab0ceb67c93aa
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-03-31 14:23:35 +0530

    Call the getDBConn() method inside the field connection.

--- src/com/wipro/candidate/bean/CandidateBean.java
+++ src/com/wipro/candidate/bean/CandidateBean.java
@@ -1,9 +1,11 @@
 package com.wipro.candidate.bean;
 
+import com.wipro.candidate.util.DBUtil;
+
 import java.sql.Connection;
 
 public class CandidateBean {
     public static void main(String[] args) {
-        Connection connection;
+        Connection connection = DBUtil.getDBConn();
     }
 }
