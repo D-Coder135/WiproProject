14609f4765e65f338215e677df1b0786d006bc9b
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-03-31 14:19:38 +0530

    Create an object of the Connection interface inside the CandidateBean.java

--- src/com/wipro/candidate/bean/CandidateBean.java
+++ src/com/wipro/candidate/bean/CandidateBean.java
@@ -1,7 +1,9 @@
 package com.wipro.candidate.bean;
 
+import java.sql.Connection;
+
 public class CandidateBean {
     public static void main(String[] args) {
-        
+        Connection connection;
     }
 }
