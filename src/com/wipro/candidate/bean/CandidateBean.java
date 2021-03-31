9a33f2fd30c5db38f4f19d7035e77f4088838176
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-03-31 14:23:58 +0530

    Print the connection field on the console.

--- src/com/wipro/candidate/bean/CandidateBean.java
+++ src/com/wipro/candidate/bean/CandidateBean.java
@@ -7,5 +7,6 @@ import java.sql.Connection;
 public class CandidateBean {
     public static void main(String[] args) {
         Connection connection = DBUtil.getDBConn();
+        System.out.println(connection);
     }
 }
