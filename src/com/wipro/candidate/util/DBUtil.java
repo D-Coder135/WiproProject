05b0d2bed4f3ad97ad13e72378c643b74f50cea0
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-03-31 13:53:58 +0530

    Add the username value for the private field of the username

--- src/com/wipro/candidate/util/DBUtil.java
+++ src/com/wipro/candidate/util/DBUtil.java
@@ -3,7 +3,7 @@ package com.wipro.candidate.util;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/Candidate Management System";
 
-    private static final String USERNAME = "";
+    private static final String USERNAME = "root";
 
     private static final String PASSWORD = "";
 }
