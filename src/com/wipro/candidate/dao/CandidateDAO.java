c5ce750e847facefc6ea88c982a90719e1014d62
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-01 23:44:46 +0530

    Add a method addCandidate() to add candidates in the database.

--- src/com/wipro/candidate/dao/CandidateDAO.java
+++ src/com/wipro/candidate/dao/CandidateDAO.java
@@ -1,4 +1,10 @@
 package com.wipro.candidate.dao;
 
+import com.wipro.candidate.bean.CandidateBean;
+
 public class CandidateDAO {
+
+    public String addCandidate(CandidateBean candidateBean) {
+
+    }
 }
