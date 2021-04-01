042478455b2f2f43725a95bc706b14dbef0c0ca8
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-01 23:52:08 +0530

    Add a final String field to store the insert query.

--- src/com/wipro/candidate/dao/CandidateDAO.java
+++ src/com/wipro/candidate/dao/CandidateDAO.java
@@ -4,7 +4,11 @@ import com.wipro.candidate.bean.CandidateBean;
 
 public class CandidateDAO {
 
+    public static final String INSERT_QUERY = "Insert Into CANDIDATE_TBL (ID, Name, M1, M2, M3, Result, Grade)" +
+            "Values (?, ?, ?, ?, ?, ?, ?)";
+
     public String addCandidate(CandidateBean candidateBean) {
+
         return "TRUE";
     }
 }
