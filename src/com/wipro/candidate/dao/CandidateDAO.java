bf89a66a3c44ef1c5772d1e26860152a12515d34
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-02 00:00:16 +0530

    add a throws statement with the method signature.

--- src/com/wipro/candidate/dao/CandidateDAO.java
+++ src/com/wipro/candidate/dao/CandidateDAO.java
@@ -3,13 +3,14 @@ package com.wipro.candidate.dao;
 import com.wipro.candidate.bean.CandidateBean;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class CandidateDAO {
 
     public static final String INSERT_QUERY = "Insert Into CANDIDATE_TBL (ID, Name, M1, M2, M3, Result, Grade)" +
             "Values (?, ?, ?, ?, ?, ?, ?)";
 
-    public String addCandidate(Connection connection, CandidateBean candidateBean) {
+    public String addCandidate(Connection connection, CandidateBean candidateBean) throws SQLException {
         String id = candidateBean.getId();
         String name = candidateBean.getName();
         int m1 = candidateBean.getM1();
