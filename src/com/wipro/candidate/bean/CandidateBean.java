3cda0b7f1ee39c1e0f36303f92fe2436e76cf274
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-01 23:35:19 +0530

    Add the getter and setter methods for all the private fields.

--- src/com/wipro/candidate/bean/CandidateBean.java
+++ src/com/wipro/candidate/bean/CandidateBean.java
@@ -9,4 +9,59 @@ public class CandidateBean {
     private String result;
     private String grade;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getM1() {
+        return m1;
+    }
+
+    public void setM1(int m1) {
+        this.m1 = m1;
+    }
+
+    public int getM2() {
+        return m2;
+    }
+
+    public void setM2(int m2) {
+        this.m2 = m2;
+    }
+
+    public int getM3() {
+        return m3;
+    }
+
+    public void setM3(int m3) {
+        this.m3 = m3;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
