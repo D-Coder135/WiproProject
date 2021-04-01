bf85bac19bdd901f51b829d8bb55f39e4c34b7ae
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-01 23:26:14 +0530

    Removed the main() method from the CandidateBean.java class

--- src/com/wipro/candidate/bean/CandidateBean.java
+++ src/com/wipro/candidate/bean/CandidateBean.java
@@ -1,12 +1,5 @@
 package com.wipro.candidate.bean;
 
-import com.wipro.candidate.util.DBUtil;
-
-import java.sql.Connection;
-
 public class CandidateBean {
-    public static void main(String[] args) {
-        Connection connection = DBUtil.getDBConn();
-        System.out.println(connection);
-    }
+
 }
