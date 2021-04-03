d4fabb0ec82dcc2c26005a0bc5c3b0fff09d8514
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-03 13:01:12 +0530

    call the charAt() method to fetch each character from the name.

--- src/com/wipro/candidate/dao/CandidateDAO.java
+++ src/com/wipro/candidate/dao/CandidateDAO.java
@@ -105,7 +105,7 @@ public class CandidateDAO {
     public String generateCandidateID(String name) {
         String finalID = "";
         for (int index = 0; index < name.length(); index++) {
-
+            char character = name.charAt(index);
         }
         return null;
     }
