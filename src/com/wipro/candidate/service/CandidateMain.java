ab7dd6a4c9789b165997ae32fcc8752bf9e85b9a
D-Coder135 <devansh.shukla_cs19@gla.ac.in>
2021-04-04 13:15:17 +0530

    add a method addCandidate() with return type String and pass an object of class CandidateBean.java as a parameter

--- src/com/wipro/candidate/service/CandidateMain.java
+++ src/com/wipro/candidate/service/CandidateMain.java
@@ -1,7 +1,13 @@
 package com.wipro.candidate.service;
 
+import com.wipro.candidate.bean.CandidateBean;
+
 public class CandidateMain {
     public static void main(String[] args) {
+
+    }
+
+    public String addCandidate(CandidateBean candidateBean) {
 
     }
 }
