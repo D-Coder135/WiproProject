8b44a36ffdf588959ac251945f5717bdeda2a4a9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-24 14:12:31 +0530

    Override the toString() method and add return statement inside the body of the method

--- src/com/wipro/candidate/util/WrongDataException.java
+++ src/com/wipro/candidate/util/WrongDataException.java
@@ -8,5 +8,9 @@
 package com.wipro.candidate.util;
 
 public class WrongDataException extends Exception {
-
+    @Override
+    public String toString() {
+        return "Data Incorrect";
+    }
 }
+
