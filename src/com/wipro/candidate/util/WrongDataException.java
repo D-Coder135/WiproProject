bab95e4082ae34debac7070245671a2b1f7cfe97
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-24 14:01:49 +0530

    Add a class WrongDataException.java inside the package candidate.util

---
+++ src/com/wipro/candidate/util/WrongDataException.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D_Coder135)
+ *   Date: 24/04/21
+ *   Time: 2:01 PM
+ *   File: WrongDataException.java
+ */
+
+package com.wipro.candidate.util;
+
+public class WrongDataException {
+}
