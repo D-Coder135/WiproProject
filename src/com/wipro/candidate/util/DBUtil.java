254eba6b5c3749316bd527b75a56c8f741518788
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-04-24 14:01:12 +0530

    Remove the inner class WrongDataException from DBUtil.java

--- src/com/wipro/candidate/util/DBUtil.java
+++ src/com/wipro/candidate/util/DBUtil.java
@@ -33,12 +33,4 @@ public class DBUtil {
         System.out.println(connection == null ? "The connection to the database is inactive." :
                 "The conncetion to the database is active.");
     }
-
-    public class WrongDataException extends Exception {
-        @Override
-        public String toString() {
-            return "Data Incorrect";
-        }
-    }
-
 }
